86d0717c0e1ec62e6e76ba1f9fb29f65becf82e4
ja-nguyen <67073372+ja-nguyen@users.noreply.github.com>
2022-08-19 17:00:33 -0500

    Update

---
+++ Reference.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pickle
+from scipy.integrate import odeint
+
+with open("promo.pkl", "rb") as fid:
+    promo = pickle.load(fid)
+
+def reference(y,t,k_end):
+    return np.array([k_end - 2.7*y[0],
+                    y[0] - 0.029*y[1]])
+
+
+Ref = dict()
+for k in promo.keys():
+    off = odeint(reference, np.zeros(2), np.arange(0, 48 + 1), args=(promo[k]['off'],))[-1,-1]
+    on = odeint(reference, np.zeros(2), np.arange(0, 48 + 1), args=(promo[k]['on'],))[-1, -1]
+    Ref.update({k: {'off': off, 'on': on}})
+
+with open("Ref.pkl", "wb") as fid:
+    pickle.dump(Ref, fid)
+
--- scratch.py
+++ scratch.py
@@ -1,70 +1,103 @@
 import numpy as np
-import pandas as pd
+# import pandas as pd
 import pickle
 import networkx as nx
-import sympy as sm
-
+from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+
 with open("promo.pkl", "rb") as fid:
     promo = pickle.load(fid)
 with open("parts.pkl", "rb") as fid:
     parts = pickle.load(fid)
+with open("Ref.pkl", "rb") as fid:
+    Ref = pickle.load(fid)
 
 class Topo():
     def __init__(self, edge_list):
         self.graph = nx.DiGraph()
         self.graph.add_edges_from(edge_list)
         self.nodes = self.graph.nodes
-        # self.in_edges = self.graph.in_edges
-        # self.out_edges = self.graph.out_edges
+        self.dose = {'Z': 200, 'I': 200, 'R': 9}
+        self.protein_deg = {'Z': 0.35, 'I': 0.35, 'R': 0.029}
+        self.node_dict = None
+        self.num_states = None
+        self.var_dict = None
 
     def plot_graph(self):
         plt.tight_layout()
         nx.draw_networkx(self.graph, arrows=True)
         plt.show()
 
-    def get_in_nodes(self):
+    def get_nodes(self):
         self.node_dict = dict()
-        # state = 0
         for n in self.nodes:
             if n[0] != "P":
                 edge = self.graph.in_edges(n)
-                self.node_dict.update({n: [i[0] for i in edge]})
-
-    def make_equations(self):
-        self.get_in_nodes()
+                # self.node_dict.update({n: [i[0] for i in edge]})
+                self.node_dict.update({n:
+                                         {'P': [i[0] for i in edge if i[0][0] == 'P'],
+                                          'Z': [i[0] for i in edge if i[0][0] == 'Z'],
+                                          'I': [i[0] for i in edge if i[0][0] == 'I']}})
         self.num_states = len(self.node_dict.keys())
-        # states_sm = ''
-        # for i in range(self.num_states):
-        #     states_sm += ('x' + str(i))
-        #     if i == self.num_states - 1:
-        #         break
-        #     states_sm += ','
-        # X = sm.Matrix(sm.symbols(states_sm))
-        self.eq_dict = dict(zip(self.node_dict.keys(), np.arange(self.num_states)))
+        self.var_dict = dict(zip(self.node_dict.keys(), np.arange(self.num_states)))
+
+    def system_equations(self, x, t, state):
+        system = []
+        for n in self.node_dict.keys():
+            eq = -2.7 * x[2 * self.var_dict[n]]
+            b = 0
+            num = 0
+            denom = 1
+            for k in self.node_dict[n]['P']:
+                eq += self.dose[n[0]] * promo[k][state]
+            for k in self.node_dict[n]['Z']:
+                b += parts[k][0]
+                num += parts[k][1] * parts[k][2] * x[2 * self.var_dict[k] + 1]
+                denom += parts[k][2] * x[2 * self.var_dict[k] + 1]
+            for k in self.node_dict[n]['I']:
+                denom += parts[k][0] * x[2 * self.var_dict[k] + 1]
+            if len(self.node_dict[n]['Z']) > 0:
+                b /= len(self.node_dict[n]['Z'])
+            eq += self.dose[n[0]] * (b + num) / denom
+            system.extend([eq, -self.protein_deg[n[0]] * x[2 * self.var_dict[n] + 1] + x[2 * self.var_dict[n]]])
+        return system
+
+    def simulate(self):
+        t = np.arange(0,48,1)
+        sol = odeint(self.system_equations, np.zeros(self.num_states*2), t, args=('off',))
+        rep_off = sol[-1,-1]
+        sol = odeint(self.system_equations, np.zeros(self.num_states*2), t, args=('on',))
+        rep_on = sol[-1,-1]
+        return rep_off, rep_on
+
+    def get_fitness(self):
+
+
+
 
 edge_list = [("P1", "Z1"), ("Z1", "Rep")]
 g1 = Topo(edge_list)
-
-def fun(x, state):
-    system = []
-    for n in g1.node_dict.keys():
-        eq = -2.7 * x[2 * g1.eq_dict[n]]
-        b = 0
-        num = 0
-        denom = 1
-        for k in eq_dict['P']:
-            eq += 200 * promo[k][state]
-        for k in eq_dict['Z']:
-            b += parts[k][0]
-            num += parts[k][1] * parts[k][2] * x[2 * g1.eq_dict[k] + 1]
-            denom += parts[k][2] * x[2 * g1.eq_dict[k] + 1]
-        for k in eq_dict['I']:
-            denom += parts[k][0] * x[2 * g1.eq_dict[k] + 1]
-        if len(eq_dict['Z']) > 0:
-            b /= len(eq['Z'])
-        eq += 200*(b + num) / denom
-        system.extend([eq, -0.35 * x[2 * g1.eq_dict[n] + 1] + x[2 * g1.eq_dict[n]]])
-    return system
+g1.get_nodes()
+# g1.simulate()
+# def fun(x, state):
+#     system = []
+#     for n in g1.node_dict.keys():
+#         eq = -2.7 * x[2 * g1.var_dict[n]]
+#         b = 0
+#         num = 0
+#         denom = 1
+#         for k in g1.node_dict[n]['P']:
+#             eq += 200 * promo[k][state]
+#         for k in g1.node_dict[n]['Z']:
+#             b += parts[k][0]
+#             num += parts[k][1] * parts[k][2] * x[2 * g1.var_dict[k] + 1]
+#             denom += parts[k][2] * x[2 * g1.var_dict[k] + 1]
+#         for k in g1.node_dict[n]['I']:
+#             denom += parts[k][0] * x[2 * g1.var_dict[k] + 1]
+#         if len(g1.node_dict[n]['Z']) > 0:
+#             b /= len(g1.node_dict[n]['Z'])
+#         eq += 200*(b + num) / denom
+#         system.extend([eq, -0.35 * x[2 * g1.var_dict[n] + 1] + x[2 * g1.var_dict[n]]])
+#     return system
 
 a = 1
