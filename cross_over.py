44d670fe6fd9aaebb3f45fb6f95198e89161a508
ja-nguyen <67073372+ja-nguyen@users.noreply.github.com>
2022-08-26 11:02:03 -0500

    Update

---
+++ cross_over.py
@@ -0,0 +1,3 @@
+from scratch import *
+
+a = 1
--- scratch.py
+++ scratch.py
@@ -20,44 +20,52 @@ class Topo():
 
         self.dose = {'Z': 200, 'I': 200, 'R': 9}
         self.protein_deg = {'Z': 0.35, 'I': 0.35, 'R': 0.029}
-        self.node_dict = dict() # Classify nodes
+        self.in_dict = dict() # Classify nodes
+        self.out_dict = dict()
         for n in self.node_list:
-            edge = self.graph.in_edges(n)
-            self.node_dict.update({n:
-                                        {'P': [i[0] for i in edge if i[0][0] == 'P'],
-                                        'Z': [i[0] for i in edge if i[0][0] == 'Z'],
-                                        'I': [i[0] for i in edge if i[0][0] == 'I']}})
-        self.num_states = len(self.node_dict.keys())
-        self.var_dict = dict(zip(self.node_dict.keys(), np.arange(self.num_states)))
+            in_edge = self.graph.in_edges(n)
+            self.in_dict.update({n:
+                                        {'P': [i[0] for i in in_edge if i[0][0] == 'P'],
+                                        'Z': [i[0] for i in in_edge if i[0][0] == 'Z'],
+                                        'I': [i[0] for i in in_edge if i[0][0] == 'I']}})
+
+            out_edge = self.graph.out_edges(n)
+            self.out_dict.update({n:
+                                     {'P': [i[0] for i in out_edge if i[0][0] == 'P'],
+                                      'Z': [i[0] for i in out_edge if i[0][0] == 'Z'],
+                                      'I': [i[0] for i in out_edge if i[0][0] == 'I']}})
+
+        self.num_states = len(self.node_list)
+        self.var_dict = dict(zip(self.in_dict.keys(), np.arange(self.num_states)))
         self.valid = None
 
     def check_valid(self):
         self.valid = 1
         for n in self.node_list:
-            if (len(self.node_dict[n]['I']) > 0) & (len(self.node_dict[n]['P']) > 0) & (len(self.node_dict[n]['Z']) == 0):
+            if (len(self.in_dict[n]['I']) > 0) & (len(self.in_dict[n]['P']) > 0) & (len(self.in_dict[n]['Z']) == 0):
                 self.valid = 0
-            elif (n != 'Rep') & ((len(self.graph.in_edges('Z1')) == 0) | (len(self.graph.out_edges('Z1')) == 0)):
+            elif (n != 'Rep') & ((len(self.graph.in_edges(n)) == 0) | (len(self.graph.out_edges(n)) == 0)):
                 self.valid = 0
         if len(self.promo) != 1:
             self.valid = 0
 
     def system_equations(self, x, t, state):
         system = []
-        for n in self.node_dict.keys():
+        for n in self.in_dict.keys():
             eq = -2.7 * x[2 * self.var_dict[n]]
             b = 0
             num = 0
             denom = 1
-            for k in self.node_dict[n]['P']:
+            for k in self.in_dict[n]['P']:
                 eq += self.dose[n[0]] * promo[k][state]
-            for k in self.node_dict[n]['Z']:
+            for k in self.in_dict[n]['Z']:
                 b += parts[k][0]
                 num += parts[k][1] * parts[k][2] * x[2 * self.var_dict[k] + 1]
                 denom += parts[k][2] * x[2 * self.var_dict[k] + 1]
-            for k in self.node_dict[n]['I']:
+            for k in self.in_dict[n]['I']:
                 denom += parts[k][0] * x[2 * self.var_dict[k] + 1]
-            if len(self.node_dict[n]['Z']) > 0:
-                b /= len(self.node_dict[n]['Z'])
+            if len(self.in_dict[n]['Z']) > 0:
+                b /= len(self.in_dict[n]['Z'])
             eq += self.dose[n[0]] * (b + num) / denom
             system.extend([eq, -self.protein_deg[n[0]] * x[2 * self.var_dict[n] + 1] + x[2 * self.var_dict[n]]])
         return system
