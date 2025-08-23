3dd76fe2df06324ae0fe5c1de1d3e14eedd88096
Kathleen (Katie) Dreyer <kathleendreyer2024@u.northwestern.edu>
2025-08-23 17:34:37 -0500

    remove outdated directory and hyperparameter_optimization/2023_summer directory (outdated files)

--- hyperparameter_optimization/2023_summer/Optimizer_Open.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-with open('Results/SetDose.pkl', 'rb') as f:
-    circuit = pd.read_pickle(f)
-
-print(circuit)
--- hyperparameter_optimization/2023_summer/define_problem.py
+++
@@ -1,9 +0,0 @@
-class Problem:
-    def __init__(self, promo_node, max_part, min_dose, max_dose, dose_interval, inhibitor, func=None):
-        self.promo_node = promo_node
-        self.max_part = max_part
-        self.min_dose = min_dose
-        self.max_dose = max_dose
-        self.dose_interval = dose_interval
-        self.inhibitor = inhibitor
-        self.func = func
